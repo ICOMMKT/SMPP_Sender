67b97cce51ab463766c83af126f29fe73899804b
nicokras <nicolas.krasny@itechla.com>
2023-08-28 11:42:08 -0300

    added fields to form

--- SMPP_Sender/Form1.Designer.cs
+++ SMPP_Sender/Form1.Designer.cs
@@ -30,13 +30,19 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.txtMobileNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(62, 36);
+            this.button1.Location = new System.Drawing.Point(46, 276);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(135, 69);
+            this.button1.Size = new System.Drawing.Size(64, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Send";
             this.button1.UseVisualStyleBackColor = true;
@@ -44,25 +50,83 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(62, 137);
+            this.button2.Location = new System.Drawing.Point(169, 276);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(135, 69);
+            this.button2.Size = new System.Drawing.Size(61, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "Close Conn";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // txtMobileNumber
+            // 
+            this.txtMobileNumber.Location = new System.Drawing.Point(45, 54);
+            this.txtMobileNumber.Name = "txtMobileNumber";
+            this.txtMobileNumber.Size = new System.Drawing.Size(207, 20);
+            this.txtMobileNumber.TabIndex = 2;
+            this.txtMobileNumber.Text = "5491132834957";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Mobile Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Message";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(45, 125);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(207, 20);
+            this.txtMessage.TabIndex = 4;
+            this.txtMessage.Text = "Prueba";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Result";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(47, 192);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(207, 66);
+            this.txtResult.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.ClientSize = new System.Drawing.Size(324, 311);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMobileNumber);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +134,12 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtMobileNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResult;
 
     }
 }
--- SMPP_Sender/Form1.cs
+++ SMPP_Sender/Form1.cs
@@ -32,23 +32,25 @@ namespace SMPP_Sender
             properties.Host = "bulksms.vsms.net"; //SMSC host name or IP Address
             properties.SystemType = "SMPP";
             properties.DefaultServiceType = "SMPP";
+            properties.DefaultEncoding = DataCoding.Latin1;*/
 
             /*Tedexis Pasaporte Maestro*/
             /*properties.SystemID = "BSAG";
             properties.Password = "PQzCM6pV";*/
 
             /*Tedexis Farmatodo*/
-            properties.SystemID = "FARMATODO";
-            properties.Password = "brHlDyUf";
+            /*properties.SystemID = "FARMATODO";
+            properties.Password = "brHlDyUf";*/
 
             /*Tedexis Perú*/
             /*properties.SystemID = "TDXTESTPER";
             properties.Password = "0zaGxnNw";*/
 
-            properties.Port = 17631; //IP port to use
+            /*properties.Port = 17631; //IP port to use
             properties.Host = "200.41.57.109"; //SMSC host name or IP Address
             properties.SystemType = "SMPP";
             properties.DefaultServiceType = "SMPP";
+            properties.DefaultEncoding = DataCoding.Latin1;*/
 
             /*MOCEAN SMS*/
             /*properties.SystemID = "icomsmpp";
@@ -56,9 +58,106 @@ namespace SMPP_Sender
             properties.Port = 28001; //IP port to use
             properties.Host = "183.81.161.84"; //SMSC host name or IP Address
             properties.SystemType = "sms";
-            properties.DefaultServiceType = "SMPP";*/
-            
+            properties.DefaultServiceType = "SMPP";
+            properties.DefaultEncoding = DataCoding.Latin1;*/
 
+            /*Infobip Itech*/
+            properties.SystemID = "ItechArgentina";
+            properties.Password = "10Itech!";
+            properties.Port = 8888; //IP port to use
+            properties.Host = "smpp3.infobip.com"; //SMSC host name or IP Address
+            properties.SystemType = "sms";
+            properties.DefaultServiceType = "SMPP";
+            properties.DefaultEncoding = DataCoding.Latin1;
+
+            /*Infobip Support*/
+            /*properties.SystemID = "soporteitech";
+            properties.Password = "1rY23BcPwg!01";
+            properties.Port = 8888; //IP port to use
+            properties.Host = "smpp3.infobip.com"; //SMSC host name or IP Address
+            properties.SystemType = "sms";
+            properties.DefaultServiceType = "SMPP";
+            properties.DefaultEncoding = DataCoding.Latin1;*/
+
+            /*Altera Chile*/
+            /*properties.SystemID = "icommtst";
+            properties.Password = "F8c0A3c1";
+            properties.Port = 55090; //IP port to use
+            properties.Host = "165.232.51.235"; //SMSC host name or IP Address
+            properties.SystemType = "sms";
+            properties.DefaultServiceType = "SMPP";
+            properties.DefaultEncoding = DataCoding.Latin1;*/
+
+            /*Infobip Iexpandit*/
+            /*properties.SystemID = "adelanto2";
+            properties.Password = "Ixpandit2018";
+            properties.Port = 8888; //IP port to use
+            properties.Host = "smpp3.infobip.com"; //SMSC host name or IP Address
+            properties.SystemType = "sms";
+            properties.DefaultServiceType = "SMPP";
+            properties.DefaultEncoding = DataCoding.Latin1;*/
+
+
+            /*Sistemas Masivos Colombia*/
+            /*properties.SystemID = "icommark";
+            properties.Password = "AhbfY68";
+            properties.Port = 8237; //IP port to use
+            properties.Host = "sistemasmasivos.com"; //SMSC host name or IP Address
+            properties.SystemType = "sms";
+            properties.DefaultServiceType = "SMPP";
+            properties.DefaultEncoding = DataCoding.Latin1;*/
+
+            /*Identidad SMS Colombia*/
+            //properties.SystemID = "icommktPrm";
+            //properties.Password = "1C0mmkt$PR";
+            /*Identidad Std*/
+            //properties.SystemID = "icommktStd";
+            //properties.Password = "1C0mmkt$";
+            /*Identidad SMS Internacional*/
+            /*properties.SystemID = "Icommkt-internacional";
+            properties.Password = "Icommkt2019";
+            properties.Port = 2875; //IP port to use
+            properties.Host = "207.101.124.227"; //SMSC host name or IP Address
+            properties.SystemType = "";
+            properties.DefaultServiceType = "SMPP";
+            properties.DefaultEncoding = DataCoding.Latin1;*/
+
+            /*TuLoEnvias Perú*/
+            /*properties.SystemID = "iconm_pe";
+            properties.Password = "qLOHfEcB";
+            properties.Port = 8888; //IP port to use
+            properties.Host = "107.20.199.106"; //SMSC host name or IP Address
+            properties.SystemType = "sms";
+            properties.DefaultServiceType = "SMPP";
+            properties.DefaultEncoding = DataCoding.Latin1;*/
+
+            /*ITELVOX Perú*/
+            /*properties.SystemID = "netv-itechla";
+            properties.Password = "1t3chL4";
+            properties.Port = 2345; //IP port to use
+            properties.Host = "121.241.242.114"; //SMSC host name or IP Address
+            properties.SystemType = "sms";
+            properties.DefaultServiceType = "SMPP";
+            properties.DefaultEncoding = DataCoding.Latin1;*/
+
+            /*Corzu.Net Chile*/
+            /*properties.SystemID = "czn_icommkt";
+            properties.Password = "imCzn18";
+            properties.Port = 10020; //IP port to use
+            properties.Host = "190.107.177.120"; //SMSC host name or IP Address
+            properties.SystemType = "";
+            properties.DefaultServiceType = "";
+            properties.DefaultEncoding = DataCoding.Latin1;*/
+
+            /*CelMedia Chile*/
+            /*properties.SystemID = "becedigmt";
+            properties.Password = "beceligmt";
+            properties.Port = 8507; //IP port to use
+            properties.Host = "200.68.3.74"; //SMSC host name or IP Address
+            properties.SystemType = "smpp";
+            properties.DefaultServiceType = "";
+            properties.DefaultEncoding = DataCoding.Latin1;*/
+                        
             //Resume a lost connection after 30 seconds
             client.AutoReconnectDelay = 3000;
 
@@ -106,26 +205,35 @@ namespace SMPP_Sender
 
                 TextMessage msg = new TextMessage();
 
-                msg.SourceAddress = "5411452382000"; //Originating number
+                msg.SourceAddress = "1152382000"; //Originating number
                 msg.RegisterDeliveryNotification = true; //I want delivery notification for this message
+                msg.SubmitUserMessageReference = true;
 
+                client.MessageSent += client_MessageSent;
                 client.MessageDelivered += client_MessageDelivered;
-
+                
                 //client.SendMessage(msg);
                 //client.BeginSendMessage(msg, SendMessageCompleteCallback, client);
 
                 /*Argentina*/
-                /*msg.Text = "Nico soy ICOMMKT";
-                msg.DestinationAddress = "5491132834957"; //Receipient number
+                msg.Text = txtMessage.Text;
+                msg.DestinationAddress = txtMobileNumber.Text;
+                msg.UserMessageReference = "REF:" + txtMobileNumber.Text + "-" + txtMessage.Text;
                 client.SendMessage(msg);
 
-                msg.Text = "Leo soy ICOMMKT";
+                /*msg.Text = "Leo soy ICOMMKT";
                 msg.DestinationAddress = "5491132834954"; //Receipient number
                 client.SendMessage(msg);*/
 
                 /*Chile*/
-                msg.Text = "Mensaje de prueba enviado por ICOMMKT, favor confirmar recepción a nicolas.krasny@itechla.com";
-                /*msg.DestinationAddress = "5691381660";
+                //msg.SourceAddress = "99994"; //Originating number
+                /*msg.Text = "Mensaje de prueba enviado por ICOMMKT, favor confirmar recepcion a nicolas.krasny@itechla.com";
+                msg.DestinationAddress = "56958733713"; /*Santiago*/
+                //client.SendMessage(msg);*/
+
+                /*Ecuador*/
+                /*msg.Text = "Mensaje de prueba enviado por ICOMMKT, favor confirmar recepción a leonardo.faigenbom@itechla.com";
+                msg.DestinationAddress = "593993396991";
                 client.SendMessage(msg);*/
                 
                 /*msg.DestinationAddress = "56977336656";
@@ -143,7 +251,7 @@ namespace SMPP_Sender
                 /*Colobia*/
                 /*msg.Text = "SMS enviado desde Icommarketing, confirmar recepción a Nico" + DateTime.Now.Hour.ToString() + ":" + DateTime.Now.Minute.ToString();
                 msg.DestinationAddress = "573012897090"; //Receipient number
-                client.SendMessage(msg); ALEX*/
+                client.SendMessage(msg); /*ALEX*/
                 /*msg.Text = "SMS enviado desde Icommarketing, confirmar recepción a Alex" + DateTime.Now.Hour.ToString() + ":" + DateTime.Now.Minute.ToString();
                 msg.DestinationAddress = "573153892047"; //Receipient number
                 client.SendMessage(msg);
@@ -168,7 +276,11 @@ namespace SMPP_Sender
                 /*msg.DestinationAddress = "582824252562";
                 client.SendMessage(msg);*/
 
-                MessageBox.Show("Enviado");
+                /*msg.Text = "SMS enviado desde Icommarketing, confirmar recepción a Nicolás Krasny (nicolas.krasny@itechla.com)" + DateTime.Now.Hour.ToString() + ":" + DateTime.Now.Minute.ToString();
+                msg.DestinationAddress = "51979411820";
+                client.SendMessage(msg);*/
+
+                //MessageBox.Show("Enviado. ID: " + msg.ReceiptedMessageId);
             }
             catch (Exception ex)
             {
@@ -186,11 +298,21 @@ namespace SMPP_Sender
             client.EndSendMessage(result);
         }
 
+        private void client_MessageSent(object sender, MessageEventArgs e)
+        {
+            var client = (SmppClient)sender;
+            Console.WriteLine("SMPP client {0} - Message Sent to: {1} {2}", client.Name, e.ShortMessage.DestinationAddress, e.ShortMessage.UserMessageReference);
+            txtResult.Text += "Sent -> ID: " + e.ShortMessage.ReceiptedMessageId + Environment.NewLine;
+            // CANDO: save sent sms
+        }
+
         private void client_MessageDelivered(object sender, MessageEventArgs e)
         {
             TextMessage msg = e.ShortMessage as TextMessage;
             //parse msg.Text for more details
-            MessageBox.Show(msg.Text, "Message Delivered");
+            //MessageBox.Show("ID: " + msg.UserMessageReference + ", Text: " + msg.Text, "Message Delivered");
+            txtResult.Text += "Delivered -> ID: " + msg.ReceiptedMessageId + " , UserMessageReference: " + msg.UserMessageReference + ", Text: " + msg.Text + Environment.NewLine;
+
         }
 
         private void Enable_Button()
@@ -202,7 +324,7 @@ namespace SMPP_Sender
         {
             client.Shutdown();
         }
-        
+
 
     }
 }
